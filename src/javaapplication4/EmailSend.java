caca5e02bf161acdda2f67d574ca2543b2b0035c
mehranghofrani <mehran.ghofrani@yahoo.com>
2016-08-01 15:35:07 +0430

    null

--- src/javaapplication4/EmailSend.java
+++ src/javaapplication4/EmailSend.java
@@ -20,51 +20,43 @@ public class EmailSend {
     
     
     
-    public static void send() {
+    public static void send(String to) {
         //////////////////////////////////////////////////
-        // Recipient's email ID needs to be mentioned.
-      String to = "abcd@gmail.com";
+      
+		final String username = "d_alihosseiny@yahoo.com";
+		final String password = "DaniAMCEIT_155";
 
-      // Sender's email ID needs to be mentioned
-      String from = "web@gmail.com";
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.mail.yahoo.com");
+		props.put("mail.smtp.port", "587");
 
-      // Assuming you are sending email from localhost
-      String host = "localhost";
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
 
-      // Get system properties
-      Properties properties = System.getProperties();
+		try {
 
-      // Setup mail server
-      properties.setProperty("mail.smtp.host", host);
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(username));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse(to));
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler,"
+				+ "\n\n No spam to my email, please!");
 
-      // Get the default Session object.
-      Session session = Session.getDefaultInstance(properties);
+			Transport.send(message);
 
-      try{
-         // Create a default MimeMessage object.
-         MimeMessage message = new MimeMessage(session);
+			System.out.println("Done");
 
-          try {
-              message.setFrom(new InternetAddress(from));
-          } catch ( Exception ex) {
-              Logger.getLogger(JavaApplication4.class.getName()).log(Level.SEVERE, null, ex);
-          }
-
-         // Set To: header field of the header.
-              message.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
-              // Set Subject: header field
-              message.setSubject("This is the Subject Line!");
-
-              // Now set the actual message
-              message.setText("This is actual message");
-
-
-         // Send message
-         Transport.send(message);
-         System.out.println("Sent message successfully....");
-      }catch (MessagingException mex) {
-         mex.printStackTrace();
-      }
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	
         ////////////////////////////
   }
     public static boolean isValidEmailAddress(String email) {
