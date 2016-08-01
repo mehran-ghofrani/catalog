a930a393d0645e57e630cfec7ab2d677f12dd1f5
mehranghofrani <mehran.ghofrani@yahoo.com>
2016-08-01 11:11:22 +0430

    class added

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -11,12 +11,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.mail.*;
-import javax.mail.internet.*;
-import javax.activation.*;
+
 
 
 /**
@@ -64,51 +59,5 @@ public class JavaApplication4 {
            return m.matches();
     }
      
-    public static void sendCatalog() {
-        //////////////////////////////////////////////////
-        // Recipient's email ID needs to be mentioned.
-      String to = "abcd@gmail.com";
-
-      // Sender's email ID needs to be mentioned
-      String from = "web@gmail.com";
-
-      // Assuming you are sending email from localhost
-      String host = "localhost";
-
-      // Get system properties
-      Properties properties = System.getProperties();
-
-      // Setup mail server
-      properties.setProperty("mail.smtp.host", host);
-
-      // Get the default Session object.
-      Session session = Session.getDefaultInstance(properties);
-
-      try{
-         // Create a default MimeMessage object.
-         MimeMessage message = new MimeMessage(session);
-
-          try {
-              message.setFrom(new InternetAddress(from));
-          } catch ( Exception ex) {
-              Logger.getLogger(JavaApplication4.class.getName()).log(Level.SEVERE, null, ex);
-          }
-
-         // Set To: header field of the header.
-              message.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
-              // Set Subject: header field
-              message.setSubject("This is the Subject Line!");
-
-              // Now set the actual message
-              message.setText("This is actual message");
-
-
-         // Send message
-         Transport.send(message);
-         System.out.println("Sent message successfully....");
-      }catch (MessagingException mex) {
-         mex.printStackTrace();
-      }
-        ////////////////////////////
-  }
+    
 }
